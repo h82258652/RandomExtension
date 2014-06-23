17a280777c2286f5086983a909e2634180423d2e
h82258652 <842053625@qq.com>
2014-06-23 13:47:59 +0800

    add missing file.

---
+++ RandomExtension.Net45/RandomExtension.Net45/RandomExtension/RandomExtension.NextBytes.cs
@@ -0,0 +1,22 @@
+﻿
+namespace System
+{
+    public partial class RandomExtension
+    {
+        public override void NextBytes(byte[] buffer)
+        {
+            base.NextBytes(buffer);
+        }
+
+        public byte[] NextBytes(int length)
+        {
+            if (length < 0)
+            {
+            }
+
+            var buffer = new byte[length];
+            NextBytes(buffer);
+            return buffer;
+        }
+    }
+}
