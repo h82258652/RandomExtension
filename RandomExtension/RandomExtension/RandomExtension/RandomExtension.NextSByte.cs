044fe8e0b4ba2cec277a112432323f2fa334b7d8
h82258652 <842053625@qq.com>
2014-06-25 09:43:43 +0800

    update
    finish nextsbyte and nextsingle

--- RandomExtension/RandomExtension/RandomExtension/RandomExtension.NextSByte.cs
+++ RandomExtension/RandomExtension/RandomExtension/RandomExtension.NextSByte.cs
@@ -6,13 +6,20 @@ namespace System
         public sbyte NextSByte()
         {
             // TODO
-            return 0;
+            return (sbyte)Next(sbyte.MaxValue);
         }
 
-        public sbyte NextSByte(bool containNavigate)
+        public sbyte NextSByte(bool containNegative)
         {
             // TODO
-            return 0;
+            if (containNegative)
+            {
+                return (sbyte)Next(sbyte.MinValue, sbyte.MaxValue);
+            }
+            else
+            {
+                return NextSByte();
+            }
         }
 
         public sbyte NextSByte(sbyte maxValue)
@@ -22,7 +29,7 @@ namespace System
             {
                 throw new ArgumentOutOfRangeException("maxValue", "maxValue 必须大于或等于零。");
             }
-            return 0;
+            return (sbyte)Next(maxValue);
         }
 
         public sbyte NextSByte(sbyte minValue, sbyte maxValue)
@@ -32,7 +39,7 @@ namespace System
             {
                 throw new ArgumentOutOfRangeException("minValue", "“minValue”不能大于 maxValue。");
             }
-            return 0;
+            return (sbyte)Next(minValue, maxValue);
         }
     }
 }
