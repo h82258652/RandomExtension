407f95848e11a45c8d51caefa87d58c5cf093e73
h82258652 <842053625@qq.com>
2014-06-25 09:27:57 +0800

    update
    add nextbiginteger.cs

---
+++ RandomExtension/RandomExtension/RandomExtension/RandomExtension.NextBigInteger.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace System
+{
+    public partial class RandomExtension
+    {
+#if !Portable
+        public BigInteger NextBigInteger(BigInteger maxValue)
+        {
+            // TODO
+            return 0;
+        }
+
+        public BigInteger NextBigInteger(BigInteger minValue, BigInteger maxValue)
+        {
+            // TODO
+            return 0;
+        }
+#endif
+    }
+}
