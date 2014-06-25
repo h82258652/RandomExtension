67213a76af7056dc7c11cf10ac58ff4580937be5
h82258652 <842053625@qq.com>
2014-06-25 09:04:32 +0800

    update
    finish next long.

--- RandomExtension/RandomExtension/RandomExtension/RandomExtension.NextInt64.cs
+++ RandomExtension/RandomExtension/RandomExtension/RandomExtension.NextInt64.cs
@@ -3,16 +3,31 @@ namespace System
 {
     public partial class RandomExtension
     {
+        /// <summary>
+        /// 返回非负随机数。
+        /// </summary>
+        /// <returns>大于等于零且小于 System.Int64.MaxValue 的 64 位带符号整数。</returns>
         public long NextInt64()
         {
-            // TODO
-            return 0;
+            return (long)Sample() * long.MaxValue;
         }
 
+        /// <summary>
+        /// 返回随机数。
+        /// </summary>
+        /// <param name="containNegative">是否包含负数。</param>
+        /// <returns>返回一个随机的 64 位带符号整数。</returns>
         public long NextInt64(bool containNegative)
         {
-            // TODO
-            return 0;
+            if (containNegative)
+            {
+                var negative = NextBoolean() ? 1L : -1L;
+                return NextInt64() * negative;
+            }
+            else
+            {
+                return NextInt64();
+            }
         }
 
         /// <summary>
@@ -30,14 +45,35 @@ namespace System
             return (long)Sample() * maxValue;
         }
 
+        /// <summary>
+        /// 返回一个指定范围内的随机数。
+        /// </summary>
+        /// <param name="minValue">返回的随机数的下界（随机数可取该下界值）。</param>
+        /// <param name="maxValue">返回的随机数的上界（随机数不能取该上界值）。maxValue 必须大于或等于 minValue。</param>
+        /// <returns>一个大于等于 minValue 且小于 maxValue 的 64 位带符号整数，即：返回的值范围包括 minValue 但不包括 maxValue。如果 minValue 等于 maxValue，则返回 minValue。</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException"><c>minValue</c> 大于 <c>maxValue</c>。</exception>
         public long NextInt64(long minValue, long maxValue)
         {
-            // TODO
             if (minValue > maxValue)
             {
                 throw new ArgumentOutOfRangeException("minValue", "“minValue”不能大于 maxValue。");
             }
-            return 0;
+            if (minValue == maxValue)
+            {
+                return minValue;
+            }
+            if ((minValue >= 0 && maxValue >= 0) || (minValue <= 0 && maxValue <= 0))
+            {
+                var range = maxValue - minValue;
+                return minValue + (long)(Sample() * range);
+            }
+            else
+            {
+                var dMinValue = (double)minValue;
+                var dMaxValue = (double)maxValue;
+                var range = dMaxValue - dMinValue;
+                return (long)(dMinValue + Sample() * range);
+            }
         }
     }
 }
