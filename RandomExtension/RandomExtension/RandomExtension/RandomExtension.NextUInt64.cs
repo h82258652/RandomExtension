6907f2177ef7003191acec61477ee45bed8d4459
h82258652 <842053625@qq.com>
2014-06-26 08:41:15 +0800

    update
    finish nextulong

--- RandomExtension/RandomExtension/RandomExtension/RandomExtension.NextUInt64.cs
+++ RandomExtension/RandomExtension/RandomExtension/RandomExtension.NextUInt64.cs
@@ -3,17 +3,24 @@ namespace System
 {
     public partial class RandomExtension
     {
+        /// <summary>
+        /// 返回非负随机数。
+        /// </summary>
+        /// <returns>大于等于零且小于 MaxValue 的 64 位无符号长整数。</returns>
         public ulong NextUInt64()
         {
-            // TODO
-            return 0;
+            return NextUInt64(ulong.MaxValue);
         }
 
+        /// <summary>
+        /// 返回一个小于所指定最大值的非负随机数。
+        /// </summary>
+        /// <param name="maxValue">要生成的随机数的上限（随机数不能取该上限值）。maxValue 必须大于或等于零。</param>
+        /// <returns>大于等于零且小于 maxValue 的 64 位无符号长整数，即：返回值的范围通常包括零但不包括 maxValue。不过，如果 maxValue 等于零，则返回 maxValue。</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException"><c>maxValue</c> 小于 0。</exception>
         public ulong NextUInt64(ulong maxValue)
         {
-            // TODO
             return NextUInt64(0, maxValue);
-            return 0;
         }
 
         /// <summary>
